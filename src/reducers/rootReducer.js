be604b5b9c4793ca029d49b65fb0b405d863e657
Faris Tangastani <ftangastani@gmail.com>
2018-10-05 16:48:14 +0200

    Comments and readme update

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import simpleReducer from './simpleReducer';
 import textReducer from './textReducer';
+
 export default combineReducers({
  simpleReducer,
  textReducer
